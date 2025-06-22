306444f2df4646fb7c92048ae8970a2a718c55e3
nitaisutradhar <nitaisutradhar650@gmail.com>
2025-06-22 17:43:54 +0600

    simple update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import Root from './layouts/Root.jsx'
-import router from './Router/Router.jsx'
+import router from './router/router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
